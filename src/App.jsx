f8fbff0b9720d6962c3156d2d06744b5191de706
muzzipri <pmuzzi70@gmail.com>
2022-07-04 07:51:36 -0400

    adicionando os codigos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <p className=''>
+        Hello Word
+      </p>
+    </div>
+  )
+}
+
+export default App
+
